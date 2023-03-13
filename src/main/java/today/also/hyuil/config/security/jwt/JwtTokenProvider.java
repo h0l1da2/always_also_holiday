b94e03607a84d7fb8599c6ce5714f3647aa7a806
h0l1da2 <whaleee@naver.com>
2023-03-13 14:57:21 +0900

    JwtTokenProvider - 토큰 생성하는 클래스 추가 application.yml 에 토큰 키, 토큰 유효시간 추가

--- src/main/java/today/also/hyuil/config/security/jwt/JwtTokenProvider.java
+++ src/main/java/today/also/hyuil/config/security/jwt/JwtTokenProvider.java
@@ -1,10 +1,16 @@
 package today.also.hyuil.config.security.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-import java.util.Base64;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 @PropertySource("classpath:application.yml")
@@ -12,11 +18,49 @@ public class JwtTokenProvider {
 
     @Value("${token.valid.time}")
     private int tokenValidTime;
-//    private final Key key;
+    private final String accessToken = "access_token";
+    private final String refreshToken = "refresh_token";
+    private final Key key;
 
     public JwtTokenProvider(@Value("${token.secret.key}") String secretKey) {
-        byte[] keyBytes = Base64.getDecoder().decode(secretKey);
+        // secretKey base64로 디코딩
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        // key 객체로 변환
+        this.key = Keys.hmacShaKeyFor(keyBytes);
     }
+
+    public String createToken(String memberId, String role) {
+        Claims claims = getClaims(accessToken, tokenValidTime);
+
+        claims.put("memberId", memberId);
+        claims.put("role", role);
+
+        return Jwts.builder()
+                .setHeaderParam("typ", "Bearer")
+                .setClaims(claims)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String createRefreshToken() {
+        Claims claims = getClaims(refreshToken, tokenValidTime * 10);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    private Claims getClaims(String tokenType,int tokenValidTime) {
+        Date now = new Date();
+
+        return Jwts.claims()
+                        .setSubject(tokenType)
+                        .setIssuedAt(now)
+                        .setExpiration(new Date(now.getTime() + tokenValidTime));
+
+    }
+
 
 }
 
